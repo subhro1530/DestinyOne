e3544cbd6bea063ddacae0827e7557bb6af3594f
Pratham Agarwal <gargpratham71@gmail.com>
2023-12-07 19:08:35 +0530

    made some fixes bc

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 // components/Navbar.js
-import { Box, Flex, Spacer, VStack, Image } from "@chakra-ui/react";
+import { Box, Flex, Spacer, VStack, Image, Divider } from "@chakra-ui/react";
 import { GiHamburgerMenu } from "react-icons/gi"; //
 import { BiSearch } from "react-icons/bi";
 
@@ -8,13 +8,15 @@ const Navbar = () => {
     <Box
       as="nav"
       position="fixed"
-      width="100vw"
+      width="90vw"
       p={4}
       bgColor="transparent"
       transition="background-color 0.3s"
       zIndex={1}
+      marginLeft="8rem"
+      marginTop="2rem"
     >
-      <Flex align="center" flexDirection="column" width="100%">
+      <Flex align="center" flexDirection="column" width="90%">
         <VStack
           spacing={4}
           align="center"
@@ -23,13 +25,13 @@ const Navbar = () => {
           width="100%"
           alignItems="center"
           justifyContent="center"
-          gap="25rem"
+          gap="20%"
         >
-          <Box flexDirection="column" width="33.33%">
+          <Box flexDirection="column" width="30%">
             <Box>Call us: +919674177512</Box>
             <Box>shaswata.ssaha@gmail.com</Box>
           </Box>
-          <Box align="center" width="33.33%">
+          <Box align="center" width="30%">
             <Image
               src="https://tradecorp.es/wp-content/uploads/2023/05/destiny-one.jpg"
               alt="Logo"
@@ -37,19 +39,19 @@ const Navbar = () => {
               height="50"
             />
           </Box>
-          <Box width="33.33%">
+          <Box width="30%" align="center">
             <BiSearch size={24} />
           </Box>
         </VStack>
-
-        {/* Spacer */}
-        <Spacer />
-
-        {/* Second Section */}
-        <Box display={{ base: "none", md: "flex" }} align="center">
-          {/* Navigation Links */}
+        <Divider borderColor="black" borderWidth="0.5px"></Divider>
+        <Box
+          display={{ base: "flex", md: "flex" }}
+          align="center"
+          height="15vh"
+          alignItems="center"
+        >
           <Flex>
-            <Box mr={4}>Work</Box>
+            <Box mr={4}>Home</Box>
             <Box mr={4}>Pages</Box>
             <Box mr={4}>Blog</Box>
             <Box mr={4}>Gallery</Box>
@@ -57,7 +59,7 @@ const Navbar = () => {
             <Box>Contact</Box>
           </Flex>
         </Box>
-
+        <Divider borderColor="black" borderWidth="0.5px" />
         {/* Hamburger Icon for Mobile */}
         <Box display={{ base: "flex", md: "none" }}>
           <GiHamburgerMenu size={24} />
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
 // pages/index.js
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
-// pages/index.js
-import { ChakraProvider, Box } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <ChakraProvider>
+    <div>
       <Navbar />
-      {/* Your Hero section goes here */}
-      <Box h="100vh" bgColor="pink">
-        {/* Your hero content goes here */}
-      </Box>
-      <Box h="100vh" bgColor="green">
-        {/* Your hero content goes here */}
-      </Box>
-      <Box h="100vh" bgColor="skyblue">
-        {/* Your hero content goes here */}
-      </Box>
+      <HeroSection/>
       {/* The rest of your page content goes here */}
-    </ChakraProvider>
+    </div>
   );
 };
 
